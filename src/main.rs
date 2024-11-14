1d71da777ed1195b2616d0ae92c077299b97e1a7
asperand <rjm99@proton.me>
2024-11-14 14:59:33 -0500

    added basic color replacement and image saving

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use image::Rgb;
+use image::codecs::png::PngEncoder;
 use image::ImageReader;
 use image::ImageBuffer;
 use std::path::Path;
@@ -20,8 +22,14 @@ fn main() {
         Ok(user_palette) => user_palette,
         Err(_) => vec![RGB{r:3,g:3,b:3},RGB{r:255,g:255,b:255}],
     };
-    let image_rgb_vec = load_file(&file_path).expect("Couldn't open file");
-    let color_replaced_image = color_replacement(image_rgb_vec,user_palette);
+    let mut image_tp = load_file(&file_path).expect("Couldn't open file"); // Get our tuple of the image sequence, height, and width.
+    let color_replaced_image = simple_color_replacement(&mut image_tp.0,user_palette); // perform a simple color replacement on our image
+    let new_raw = to_raw_from_rgb(color_replaced_image); // create a raw sequence of u8 from our object.
+    let new_buffer: ImageBuffer<Rgb<u8>, _> =ImageBuffer::from_raw(image_tp.2,image_tp.1,new_raw).unwrap();
+    let _ = match new_buffer.save("./dither.png") {
+        Err(_) => println!("Couldn't save image buffer"),
+        Ok(_) => println!("Saved image buffer to dither.png"),
+    };
 }
 
 
@@ -76,13 +84,16 @@ where P: AsRef<Path>, {
 /// This program uses rgb::RGB pixels so we can actually
 /// do math on them through rgb's functions.
 
-fn load_file(file_path : &String ) -> Result<Vec<rgb::Rgb<u8>>,image::error::ImageError>{
+fn load_file(file_path : &String ) -> Result<(Vec<rgb::Rgb<u8>>, u32 ,u32),image::error::ImageError>{
     let mut image_rgb_vec = Vec::new();
-    let image_file = open(file_path)?.to_rgb8().into_raw(); // Converting DynamicImage into a raw u8 sequence.
-    for i in (0..image_file.len()).step_by(3) { // For each 3 channel groupings, put them into a Vec.
-        image_rgb_vec.push(RGB{r:image_file[i],g:image_file[i+1],b:image_file[i+2]});
+    let image_file = open(file_path)?.to_rgb8();
+    let image_height = image_file.height();
+    let image_width = image_file.width(); // We need these as the raw sequence doesn't have h/w
+    let image_raw = image_file.into_raw(); // Converting DynamicImage into a raw u8 sequence.
+    for i in (0..image_raw.len()).step_by(3) { // For each 3 channel groupings, put them into a Vec.
+        image_rgb_vec.push(RGB{r:image_raw[i],g:image_raw[i+1],b:image_raw[i+2]});
     }
-    Ok(image_rgb_vec)
+    Ok((image_rgb_vec,image_height,image_width)) // Returning vec of invididual RGB values
 }
 /// Finds the nearest color in the given palette.
 ///
@@ -92,35 +103,57 @@ fn load_file(file_path : &String ) -> Result<Vec<rgb::Rgb<u8>>,image::error::Ima
 ///
 /// With standard color weighting: r*0.3, g*0.59, b*0.11.
 
-fn find_nearest_color(current_color:RGB<u8>,user_palette:Vec<RGB<u8>>){
-
-
-
+fn find_nearest_color(current_color:RGB<u8>,user_palette:Vec<RGB<u8>>) -> RGB<u8> {
+    let mut lowest = 0;
+    let mut max_distance = 441.672956; // max possible distance in a 256x256x256 box
+    for i in 0..user_palette.len() {
+        // monster line incoming
+        let eu_distance = (((current_color.r as f32 - user_palette[i].r as f32) * 0.3).powi(2)+((current_color.g as f32 - user_palette[i].g as f32) * 0.59).powi(2)+((current_color.b as f32 - user_palette[i].b as f32) * 0.11).powi(2)).sqrt();
+        if eu_distance < max_distance {
+            max_distance = eu_distance;
+            lowest = i;
+        }        
+    }
+    return user_palette[lowest] // return our new color
 }
 
 /// The function that replaces colors of an image to their nearest palette pairing.
 ///
-/// First converts the PhotonImage into raw pixels (a vector of u8 values)
-/// Then, iterates over and parses out those pixels, and sends them to our
-/// find_nearest_color() function, which then returns a brand new pixel.
-/// Then, we replace the previous values with the new pixel values.
+/// Uses find_nearest_color per pixel.
+
+fn simple_color_replacement(image_rgb_vec:&mut Vec<RGB<u8>>,user_palette:Vec<RGB<u8>>) -> Vec<RGB<u8>> {
+    for i in 0..image_rgb_vec.len(){
+        image_rgb_vec[i] = find_nearest_color(image_rgb_vec[i],user_palette.clone());
+    }
+    return image_rgb_vec.to_vec()
+}
+
+/// TODO:
+/// This might be difficult. We need to ensure we are hitting 
+/// the right pixels when doing our error diffusion because
+/// the image is in a raw sequence of pixels. Thus, we cannot
+/// refer to x/y coordinates alone.
 ///
-/// Repeat ad nausem
+///
+/// 0  1  2  3  4  5  6
+/// 7  8  9  10 11 12 13
+/// 14 15 16 17 18 19 20
+/// 21 22 23 24 25 26 27
+///
+/// 
+fn dither_image(image_rgb_vec:Vec<RGB<u8>>,height:u32,width:u32, user_palette:Vec<RGB<u8>>){
+    
 
-fn color_replacement(image_rgb_vec:Vec<RGB<u8>>,user_palette:Vec<RGB<u8>>){
- // stepping by four to group pixel.
-        /* PSEUDOCODE
-            
-            GRAB CURRENT PIXEL VALUES FROM RAW_IMAGE
-            PASS TO FIND_NEAREST_COLOR, WHICH RETURNS A SINGLE COLOR
+}
 
-            1. EDIT RAW_IMAGE AND REPLACE VALUES
+/// Convert our vector of RGBs into a raw u8 sequence that the Image crate can work with.
 
-            2. CREATE A NEW RAW IMAGE VECTOR
-
-
-
-        */
-
-    // FINALLY, CREATE NEW IMAGE FROM NEW RAW DATA
+fn to_raw_from_rgb(image_rgb_vec:Vec<RGB<u8>>) -> Vec<u8> {
+    let mut raw_sequence = Vec::new();
+    for pixel in image_rgb_vec {
+        raw_sequence.push(pixel.r);
+        raw_sequence.push(pixel.g);
+        raw_sequence.push(pixel.b);
+    }
+    return raw_sequence
 }
